e722e41d421caa473191b6375014767e0ae008b7
Unknown <GladwinPrinceC6902@gmail.com>
2017-08-09 21:08:24 +0530

    Basic additions to paint background tiles in Canvas
    Added:
    Declaration of Enemies
    Declaration of Player
    Declaration of Player.update function

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(positionX, positionY, movementSpeed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+
+    //Initializing X-axis and Y-axis position of the enemy
+    this.posX = positionX;
+    this.posY = positionY;
+    //Initializing movement speed of enemy
+    this.moveSpeed = movementSpeed;
+
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -24,13 +31,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(positionX, positionY, movementX, movementY) {
+    //Initializing X-axis and Y-axis position of the player
+    this.posX = positionX;
+    this.posY = positionY;
+    //Initializing X-axis and Y-axis movement distance of player
+    this.moveX = movementX;
+    this.moveY = movementY;
 
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+    //Player object doesn't need constant updation of properties. Hence, This function is empty
+    //This function is declared to workaround the JS error for missing update function.
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [];
+player = new Player(0,0,0,0);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
